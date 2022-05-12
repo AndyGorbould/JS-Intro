3721480dc1054701488970e55a65b24f7e29ffdc
Andy Gorbould <lost@MacBook-Air.local>
2022-05-12 16:20:18 +0200

    07-02; fetch details completed

--- 07-fetch/02-details/script.js
+++ 07-fetch/02-details/script.js
@@ -1,79 +1,60 @@
-/* becode/javascript
- *
- * /09-fetch/03-details/script.js - 11.3: details
- *
- * coded by leny@BeCode
- * started at 12/05/2019
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
-(() => {
-    // your code here
-})();
-
-
-
-
 // run button
 let run = document.getElementById("run");
 run.addEventListener("click", getX);
-
-
-
-
-
-// place to write to!
-let target = document.getElementById("target");
-
-
-
 
 // make the api path as variable
 let url = "http://localhost:5500/_shared/api.json"
 
 // make the "grab from" function
-
 async function getX() {
+
+    // get user input
+    // regex is checking if input is a number
+    let regex = /^\d+$/;
+
+    let input = document.getElementById("hero-id").value;
+    // ! is asking if this is NOT a number
+    if (!input.match(regex)) {
+        alert("not number :(");
+        return;
+    }
+
+    let i = parseInt(input);
 
     // fetch the data
     let response = await fetch(url);
 
-    // make data into json (readable)
+    // catch an error
+    if (!response.ok) {
+        let message = `SOMETHING BAD!: ${response.status}`;
+        alert(`SOMETHING BAD!: ${response.status}`);
+        throw new Error(message);
+    }
+
+    // convert data into json (readable)
     let data = await response.json();
 
     // show in console
     console.log(data);
 
+    let dude = data.heroes[i - 1];
+    if (i > data.heroes.length || i < 1) {
+        alert("I need more heros");
+    } else {
 
+        console.log(dude);
 
-    // write to target
-    document.getElementById("target").innerHTML = "XXXXXXMMMMMMEEEEENNN";
-    //= data[name[1]];
+        // this looks for the template
+        let template = document.getElementById("tpl-hero");
+        let copyTemplate = template.content.cloneNode(true);
 
+        // write content
+        // copyTemplate.querySelector(".title").textContent = (dude.id);
+        copyTemplate.querySelector(".name").textContent = (dude.name);
+        copyTemplate.querySelector(".alter-ego").textContent = (dude.alterEgo);
+        copyTemplate.querySelector(".powers").textContent = (dude.abilities);
 
-    
+        // write content to doc
+        document.getElementById("target").appendChild(copyTemplate);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// return key & value to 'li' - "hero-id"
-
-
-// abilities: (2) ['Optic force blasts', 'Master tactician']
-// alterEgo: "Scott Summers"
-// id: 1
-// name: "Cyclops"
